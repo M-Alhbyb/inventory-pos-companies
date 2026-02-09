8270873065dda85390318006c9a716d66ac709f6
Mohammed Alhbyb <mohammedalhbyb@gmail.com>
2026-02-09 14:40:52 +0200

    full features

--- inventory/settings.py
+++ inventory/settings.py
@@ -92,17 +92,17 @@ DATABASES = {
 }
 
 # RESTORE_POINT: PostgreSQL Configuration
-#if os.environ.get('DB_NAME'):
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql',
-#            'NAME': os.environ.get('DB_NAME'),
-#            'USER': os.environ.get('DB_USER'),
-#            'PASSWORD': os.environ.get('DB_PASSWORD'),
-#            'HOST': os.environ.get('DB_HOST', 'localhost'),
-#            'PORT': os.environ.get('DB_PORT', '5432'),
-#        }
-#    }
+if os.environ.get('DB_NAME'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST', 'localhost'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
